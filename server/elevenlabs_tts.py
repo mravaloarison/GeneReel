45fc2e01501912e59bb976180a9517f2fd76e979
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2024-10-08 19:34:20 -0400

    setting up elevenlabs done

--- server/elevenlabs_tts.py
+++ server/elevenlabs_tts.py
@@ -1,19 +1,52 @@
 import requests, os
 from dotenv import load_dotenv
-from config import ELEVENLABS_API_KEY
+
+from elevenlabs import VoiceSettings
+from elevenlabs.client import ElevenLabs
 
 load_dotenv()
 
-url = "https://api.elevenlabs.io/v1/voices"
+def get_voices():
+    url = "https://api.elevenlabs.io/v1/voices"
 
-headers = {
-  "Accept": "application/json",
-  "xi-api-key": os.getenv("ELEVENLABS_API_KEY"),
-  "Content-Type": "application/json"
-}
+    headers = {
+        "Accept": "application/json",
+        "xi-api-key": os.getenv("ELEVENLABS_API_KEY"),
+        "Content-Type": "application/json"
+    }
 
-response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers)
 
-data = response.json()
-for voice in data['voices']:
-    print(f"{voice['name']}; {voice['voice_id']}")
+    data = response.json()
+    for voice in data['voices']:
+        print(f"{voice['name']}: {voice['voice_id']}")
+
+
+def text_to_speech_file(text: str) -> str:
+    client = ElevenLabs(
+        api_key = os.getenv("ELEVENLABS_API_KEY"),
+    )
+
+    response = client.text_to_speech.convert(
+        voice_id = os.getenv("ELEVENLABS_VOICE_ID"), 
+        output_format = "mp3_22050_32",
+        text = text,
+        model_id = "eleven_turbo_v2_5", 
+        voice_settings = VoiceSettings(
+            stability = 0.5,
+            similarity_boost = 0.8,
+            style = 1.0,
+            use_speaker_boost = True,
+        ),
+    )
+
+    save_file_path = "static/audio.mp3"
+
+    with open(save_file_path, "wb") as f:
+        for chunk in response:
+            if chunk:
+                f.write(chunk)
+
+    return save_file_path
+
+# text_to_speech_file("Hello, my name is John. I am a software engineer.")
--- server/gemini.py
+++ server/gemini.py
@@ -1,13 +1,16 @@
 import google.generativeai as genai
 import os, json
+from dotenv import load_dotenv
+
+load_dotenv()
 
 from config import generate_json_response_prompt, get_keywords_for_each_sentence_prompt
 
-genai.configure(api_key=os.getenv("API_KEY"))
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 prompt_to_json_model = genai.GenerativeModel("gemini-1.5-flash", 
     generation_config = {"response_mime_type": "application/json"})
-prompt_to_text_model = genai.GenerativeModel("gemini-1.5-flash")
+regular_model = genai.GenerativeModel("gemini-1.5-flash")
 
 def generate_json_script(userPrompt):
     prompt = generate_json_response_prompt + userPrompt
